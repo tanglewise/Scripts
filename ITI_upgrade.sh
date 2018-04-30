5a640c7a0a3acd19f2426dbbbf2ccd1df8d29dd2
tanglewise <tanglewise@mail.com>
2018-04-30 15:27:15 -0500

    Another update

--- ITI_upgrade.sh
+++ ITI_upgrade.sh
@@ -6,11 +6,11 @@ cd /home/iota
 sudo git clone https://github.com/tanglewise/ITI.git
 cd ITI
 sudo git pull origin master
-cd /home/iota/tanglewise/iri
+cd /home/iota/ITI/iri
 sudo mvn clean compile
 sudo mvn package
 sudo rm /home/iota/node/iri-1.4.2.4.jar
-sudo cp /home/iota/tanglewise/iri/target/iri-1.4.2.4.jar /home/iota/node/
+sudo cp /home/iota/ITI/iri/target/iri-1.4.2.4.jar /home/iota/node/
 
 #Restart node
 sudo service iota stop && sudo service nelson stop && sudo service iotapm stop
