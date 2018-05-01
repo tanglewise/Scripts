218a78dfd7a2247eba2a0e2e16a71957c4be28e9
tanglewise <tanglewise@mail.com>
2018-05-01 13:31:00 -0500

    Format ITI upgrade

--- ITI_upgrade.sh
+++ ITI_upgrade.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
+sudo service iota stop && sudo service nelson stop && sudo service iotapm stop
 sudo apt-get install maven
-
 cd /home/iota
 sudo git clone https://github.com/tanglewise/ITI.git
 cd ITI
@@ -12,7 +11,5 @@ sudo mvn clean compile
 sudo mvn package
 sudo rm /home/iota/node/iri-1.4.2.4.jar
 sudo cp /home/iota/ITI/iri/target/iri-1.4.2.4.jar /home/iota/node/
-
 #Restart node
-sudo service iota stop && sudo service nelson stop && sudo service iotapm stop
 sudo service iota start && sudo service nelson start && sudo service iotapm start
