d797cdf50061fd14f3715ccba53adb1a19ecc57f
tanglewise <38637098+tanglewise@users.noreply.github.com>
2018-06-15 18:54:48 -0500

    Update IRI_node_setup.sh

--- IRI_node_setup.sh
+++ IRI_node_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Download required packages
+sudo apt-get install libc6-i386
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
